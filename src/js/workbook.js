f544b830ab5ae3b96f03bd12747e9d65f82ed0e4
vokur13 <vokur13@gmail.com>
2022-09-07 20:07:06 +0300

    task added

--- src/js/workbook.js
+++ src/js/workbook.js
@@ -1,1 +1,128 @@
+// Change this number to fetch different post
+// const postId = 1;
 
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// const postToAdd = {
+//   author: 'Mango',
+//   body: 'CRUD is awesome',
+// };
+
+// const options = {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// fetch('https://jsonplaceholder.typicode.com/posts', options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// Change value of id property to update different post
+// const postToUpdate = {
+//   id: 1,
+//   body: 'CRUD is really awesome',
+// };
+
+// const options = {
+//   method: 'PATCH',
+//   body: JSON.stringify(postToUpdate),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postToUpdate.id}`, options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log('ERROR' + error));
+
+// const postIdToDelete = 1;
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
+//   method: 'DELETE',
+// })
+//   .then(() => console.log('Post deleted'))
+//   .catch(error => console.log('Error:', error));
+
+//
+// const fetchFriends = async () => {
+//   const token = await fetch('my-api.com/me');
+//   const user = await fetch(`my-api.com/profile?token=${token}`);
+//   const friends = await fetch(`my-api.com/users/${user.id}/friends`);
+//   return friends;
+// };
+
+// fetchFriends()
+//   .then(friends => console.log(friends))
+//   .catch(error => console.error(error));
+
+//
+// const fetchUsers = async () => {
+//   const baseUrl = 'https://jsonplaceholder.typicode.com';
+//   const firstResponse = await fetch(`${baseUrl}/users/1`);
+//   const secondResponse = await fetch(`${baseUrl}/users/2`);
+//   const thirdResponse = await fetch(`${baseUrl}/users/3`);
+
+//   const firstUser = await firstResponse.json();
+//   const secondUser = await secondResponse.json();
+//   const thirdUser = await thirdResponse.json();
+
+//   console.log(firstUser, secondUser, thirdUser);
+// };
+
+// fetchUsers();
+
+//
+// const fetchUsers = async () => {
+//   const baseUrl = 'https://jsonplaceholder.typicode.com';
+//   const userIds = [1, 2, 3];
+
+//   // 1. Создаём массив промисов
+//   const arrayOfPromises = userIds.map(async userId => {
+//     const response = await fetch(`${baseUrl}/users/${userId}`);
+//     return response.json();
+//   });
+
+//   // 2. Запускаем все промисы параллельно и ждем их завершения
+//   const users = await Promise.all(arrayOfPromises);
+//   console.log(users);
+// };
+
+// fetchUsers();
+
+//
+// console.log('Hello World!');
+// const a = 5;
+
+// try {
+//   console.log('Inside try befor myVar');
+// setTimeout(() => {
+//   try {
+//     myVar;
+//   } catch (error) {
+//     console.log('Error');
+//   }
+// }, 1000);
+
+//   console.log('Inside try after myVar');
+// } catch (error) {
+//   console.log('Error');
+// }
+
+try {
+  console.log('Inside try befor myVar');
+  myVar;
+  console.log('Inside try after myVar');
+} catch (error) {
+  console.dir(error.name);
+  console.dir(error.stack);
+  console.log('Error');
+}
+console.log('After try...catch');
