b06301e41ee2210a848731a47eebf41a0f6d8515
vokur13 <103522917+vokur13@users.noreply.github.com>
2022-09-07 14:00:14 +0000

    Deploying to gh-pages from @ vokur13/goit-js-mod-11-21@882e34f202e524c4bff9b58bdfdcf3eeba860a11 🚀

---
+++ html/workbook.4cbeec6a.js
@@ -0,0 +1,2 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},o={},r=e.parcelRequireca96;null==r&&((r=function(e){if(e in n)return n[e].exports;if(e in o){var r=o[e];delete o[e];var i={id:e,exports:{}};return n[e]=i,r.call(i.exports,i,i.exports),i.exports}var l=new Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,n){o[e]=n},e.parcelRequireca96=r),r.register("9oln0",(function(e,n){})),r("9oln0");
+//# sourceMappingURL=workbook.4cbeec6a.js.map
